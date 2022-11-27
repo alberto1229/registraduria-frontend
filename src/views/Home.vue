b78215e64e5126010e9a0c6451b67b4a587b4ebc
Alberto Garcia <alberto1229@gmail.com>
2022-11-27 18:40:36 -0500

    se agrega el front a git

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  
+<div class="container my-24 px-6 mx-auto">
+<section class="mb-32 text-gray-800">
+  <div class="container mx-auto xl:px-32 text-center lg:text-center">
+    <div class="grid lg:grid-cols-2 items-center">
+      <div class="mb-12 lg:mb-0">
+        <div class="block rounded-3xl shadow-lg px-6 py-12 md:px-12 lg:-mr-14"
+          style="background: hsla(0, 0%, 100%, 0.55); backdrop-filter: blur(20px)">
+          <h2 class="text-3xl font-bold mb-6">Registraduria - Elecciones 2022 - Min Tic</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+</div>
+</template>
+
+<script>
+</script>
